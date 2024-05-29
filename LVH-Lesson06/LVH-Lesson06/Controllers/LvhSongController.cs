4b5b435187048ebb5bb2ac0eddf84ac0abacd337
levanhoang07 <levanhoang742004@gmail.com>
2024-05-29 11:04:01 +0700

    ls6

---
+++ LVH-Lesson06/LVH-Lesson06/Controllers/LvhSongController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LVH_Lesson06.Models;
+
+namespace LVH_Lesson06.Controllers
+{
+    public class LvhSongController : Controller
+    {
+        private static List<LvhSong> lvhSongs = new List<LvhSong>()
+        {
+            new LvhSong{Id=2210900024,LvhTitle="Lê Văn Hoàng",LvhAuthor="K22CNT3",LvhArtist="NTU",LvhYearlease=2020},
+             new LvhSong{Id=1,LvhTitle="Lê Văn Hoàng",LvhAuthor="K22CNT3",LvhArtist="NTU",LvhYearlease=2020},
+        };
+        // GET: LvhSong
+        /// <summary>
+        /// Hiển thị danh sachs bài hát
+        /// Author:Lê Văn Hoàng
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult LvhIndex()
+        {
+            return View(lvhSongs);
+        }
+        //Get:LvhCreate
+        /// <summary>
+        /// Form thêm mới bài hát
+        /// Author: Lê Văn Hoàng
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult LvhCreate()
+        {
+            var LvhSong = new LvhSong();
+            return View(LvhSong);
+        }
+    }
+}
---
+++ LVH-Lesson06/LVH-Lesson06/Models/LvhSong.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace LVH_Lesson06.Models
+{
+    public class LvhSong
+    {
+        [Key]
+        public double Id {get;set; }
+        [Required(ErrorMessage="Lvh: Hãy nhập tiêu đề")]
+        [DisplayName("Tiêu đề")]
+        public string LvhTitle { get; set; }
+        [Required(ErrorMessage = "Lvh: Hãy nhập tác giả")]
+        [DisplayName("Tác giả")]
+        public string LvhAuthor{ get; set; }
+        [Required(ErrorMessage = "Lvh: Hãy nhập nghệ sĩ")]
+        [StringLength(50,MinimumLength =2, ErrorMessage = "Lvh: Tên nghệ sĩ tối thiểu 2 ký tự, tối đa 50 ký tự")]
+        [DisplayName("Nghệ sĩ")]
+        public string LvhArtist { get; set; }
+        [Required(ErrorMessage = "Lvh: Hãy nhập năm xuất bản")]
+        [RegularExpression(@"[0-9],{4}",ErrorMessage ="Lvh: Nhập năm xuất bản có 4 ký tự số")]
+        [Range (1900,2024,ErrorMessage ="Năm xuất bản khoảng năm 1900 - 2024")]
+        [DisplayName("Năm xuất bản")]
+        public int  LvhYearlease { get; set; }
+
+    }
+}
